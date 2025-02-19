73e046ec9ddf5d304bf8ebc58b83eb8ea4deba6c
wiley-cotton <thomaswileycotton@gmail.com>
2025-02-19 15:59:39 -0700

    wip

--- keyboards/planck/rev6_drop/keymaps/wileycotton/config.h
+++ keyboards/planck/rev6_drop/keymaps/wileycotton/config.h
@@ -34,6 +34,9 @@
 
 #define MIDI_BASIC
 
+#define ONESHOT_TAP_TOGGLE 5  /* Tapping this number of times holds the key until tapped once again. */
+#define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
+
 /* enable advanced MIDI features:
    - MIDI notes can be added to the keymap
    - Octave shift and transpose
